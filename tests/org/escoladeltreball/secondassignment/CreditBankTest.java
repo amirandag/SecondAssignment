0cc9091c514a0b000a70e37a116da3315b73ce78
alex <alex@localhost>
2017-10-05 13:00:56 +0200

    tests created

---
+++ tests/org/escoladeltreball/secondassignment/CreditBankTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package org.escoladeltreball.secondassignment;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author alex
+ *
+ */
+public class CreditBankTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.CreditBank#withdraw(double)}.
+	 */
+	@Test
+	public void testWithdraw() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.CreditBank#isBalanceNegative()}.
+	 */
+	@Test
+	public void testIsBalanceNegative() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#deposit(double)}.
+	 */
+	@Test
+	public void testDeposit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#toDollars()}.
+	 */
+	@Test
+	public void testToDollars() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link org.escoladeltreball.secondassignment.BankImpl#getBalance()}.
+	 */
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+}
